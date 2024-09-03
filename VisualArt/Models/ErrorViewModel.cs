62db057db8e95d80f392fa99ca91ddc3956740fa
Marie-Henrie <marie.sonninen@gmail.com>
2024-09-03 16:51:09 +0300

    Add project files.

---
+++ VisualArt/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VisualArt.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
